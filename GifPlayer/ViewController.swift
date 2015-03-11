592c7bf36835bcd8654b667f09c38b1d1c07556d
Diego Guajardo <diego.guaj@gmail.com>
2015-03-11 15:04:46 +1030

    Import UIImage+animatedGIF.h and .m

--- GifPlayer/ViewController.swift
+++ GifPlayer/ViewController.swift
@@ -13,13 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var testImage = UIImage.animatedImageWithAnimatedGIFData(
+            NSData.dataWithContentsOfURL(url))
+        self.dataImageView.animationImages = testImage.images
+        self.dataImageView.animationDuration = testImage.duration
+        self.dataImageView.animationRepeatCount = 1
+        self.dataImageView.image = testImage.images.lastObject
+        self.dataImageView.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
